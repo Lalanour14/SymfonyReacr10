afc89ef098f6ca5774e61ad0ba8ec81ebaa781aa
Lalanour14 <bouzidselim@gmail.com>
2024-01-11 21:11:19 +0100

    suite

--- assets/composant/produit/listproduit.js
+++ assets/composant/produit/listproduit.js
@@ -3,9 +3,9 @@ import React from "react";
 const lits = props => {
     const nameList =props.nameList;
     const data = nameList.map (data =>{
-        return (
+        return 
             
-        )
+        
     })
 }
 
