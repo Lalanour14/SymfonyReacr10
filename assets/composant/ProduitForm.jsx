1ecc6857f65bc58f1f1b8dee6db86223645fa262
Lalanour14 <bouzidselim@gmail.com>
2024-01-22 20:54:51 +0100

    post prodruits

--- assets/composant/ProduitForm.jsx
+++ assets/composant/ProduitForm.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 
 import { useState } from "react";
-let nextId = 4;
+let nextId = "";
 export default function ProduitForm ({handleAdd}){
     //state (état, données)
-    const [form, setForm] = useState ({name :"", valeur:"", type:"",userId:""});
+    const [form, setForm] = useState ({name :"", value:"", type:"", userId:""});
     // comportement
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
 
         e.preventDefault();
         // alert("handleSubmit");
-    
+        try {
+          // effectuer une requête Post vers API avec les donnèes du nouvelle objet
+          const reponse = await fetch("http://localhost:8000/api/possesions.json",{
+          method: 'POST',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body: JSON.stringify(form),
+        
+          });
+          if(reponse.ok){
+            //Le POST a réussi,vous gérer la réponse ici
+            console.log('Objet crée avec succés');
+          }else{
+            // le POST a échoué , gérer les erreurs ici 
+            console.error('Error lors de la création de l\'objet ');
+
+          } 
+         }catch(erreur){
+            console.error('Erreur lors de la requête POST:',erreur)
+
+          }
+     ; 
       
       // 1 copie de state 
       //const ProduitsCopy = [...produits];
     
         // 2 manipulation sur copie state
         const name = form.name
-        const value = form.valeur
+        const value = form.value
         const type = form.type
         const userId = form.userId
-      const id =  nextId++;
+      const id =  "";
     let produitAjouter ={id , name , value , type , userId};
     //console.log(produitAjouter);
     //ProduitsCopy.push(produitAjouter);
-    handleAdd(produitAjouter);
+      handleAdd(produitAjouter);
         // 3 modifier le state avec le setter
     
       
-       setForm({name :"", valeur:"", type:"",userId:""});
+       setForm({name :"", value:"", type:"",userId:""});
      };
         
 
@@ -47,11 +69,11 @@ export default function ProduitForm ({handleAdd}){
      
      <input  name='value' type='text' placeholder='ajouter value'
      
-     value={form.valeur}
+     value={form.value}
      onChange={e => {
        setForm({
          ...form,
-         valeur: e.target.value
+         value: e.target.value
        });
      }}/>
      
--- src/Entity/Possesion.php
+++ src/Entity/Possesion.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\PossesionRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
 
 #[ORM\Entity(repositoryClass: PossesionRepository::class)]
 #[ApiResource]
